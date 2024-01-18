a8ac46d05e81cef5aac108be4ecd468fff99175d
Guilherme Modena <151776864+ModenaG@users.noreply.github.com>
2024-01-18 14:12:39 -0300

    Create novo 1.cpp

---
+++ novo 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+	float Num1, Num2, soma;
+
+
+	std::cout << "Digite o primeiro numero" << "\n";
+	std::cin >> Num1;
+	std::cout << "Digite o segundo numero" << "\n";
+	std::cin >> Num2;
+
+
+	soma = Num1 + Num2;
+	std::cout << "O resultado e: " << soma << "\n";
+
+	return 0;
+}
